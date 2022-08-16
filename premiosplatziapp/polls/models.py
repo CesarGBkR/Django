f82b1f5a62582d21e4e756a362ec9a95c006f756
CesarGBkR <yexotortuesprin@gmail.com>
2022-08-15 22:40:17 -0500

    Generic View

--- premiosplatziapp/polls/models.py
+++ premiosplatziapp/polls/models.py
@@ -19,7 +19,7 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=250)
-    votes = models.ImageField(default=0)
+    votes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.choice_text
